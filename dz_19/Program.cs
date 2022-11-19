5baf9dff616c75b246aa873f92019dcab009f895
Stas <stas.krivosheev.0054@gmail.com>
2022-11-19 18:24:14 +0300

    first commit

---
+++ dz_19/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+//введите число
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a3 = number /10 % 10;
+int a4 = number % 10;
+int result = a4 * 10 + a3;
+if (result == number / 1000)
+{
+  Console.WriteLine($"число {number} является палиндромом");
+}
+else Console.WriteLine($"число {number} не является палиндромом");
---
+++ dz_23/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("введите число:");
+var num = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= num; i++)
+{                                //возводим в куб
+    Console.WriteLine(Math.Pow(i,3));
+}
+
+
+
+//через вайл
+
+
+Console.Write("Введите число ");
+var number = Convert.ToInt32(Console.ReadLine());
+int number2 = 1;
+
+while (number2 <= number)
+{
+    Console.WriteLine(number2 * number2 * number2);
+number2++;
+}
